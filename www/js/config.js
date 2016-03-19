a860976e81f24c690b8dc5fa40d150429667c077
ServiApps <apps@serviapps.com>
2016-03-19 10:44:42 +0100

    SENDER ID
    SENDER ID

--- www/js/config.js
+++ www/js/config.js
@@ -2,5 +2,5 @@
 var krms_config ={			
 	'ApiUrl':"http://restakeaway.com/merchantapp/api",	
 	'DialogDefaultTitle':"RESTAKEAWAY MERCHANT APP",
-	'pushNotificationSenderid':"AIzaSyBRVmsxGerFrVD5BYvAgGoOxTDm4SRe0T4"
+	'pushNotificationSenderid':"565305536100"
 };
