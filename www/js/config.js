c1767416de46bf15f04af906ea07e6ff7c9213bd
ServiApps <apps@serviapps.com>
2016-03-19 10:11:46 +0100

    Versió Inicial Merchant App
    Versió Inicial Merchant App

---
+++ www/js/config.js
@@ -0,0 +1,6 @@
+
+var krms_config ={			
+	'ApiUrl':"http://restakeaway.com/merchantapp/api",	
+	'DialogDefaultTitle':"RESTAKEAWAY MERCHANT APP",
+	'pushNotificationSenderid':"YOUR_ANDROID_PUSH_PROJECT_ID"
+};
